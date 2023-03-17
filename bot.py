1e7154aa8df1394d6afb81a7f9354403c6f56293
vikalg <vikalg@yandex.ru>
2023-03-17 12:43:28 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = os.getenv(BOT_TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def echo(message: types.Message):
+   await message.reply('Start')
+
+
+@dp.message_handler(commands=['help'])
+async def echo(message: types.Message):
+   await message.reply('Вы обратились к справке бота')
+
+
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
